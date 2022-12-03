20e4819ee804c9f1bead8aa6d94ccb28728fc5f4
Luke G <lagodw@gmail.com>
2022-12-03 11:11:31 -0500

    starting spawn location

--- units/Army.gd
+++ units/Army.gd
@@ -89,14 +89,14 @@ func _change_state(new_state):
 func _process(delta):
 	if player != Global.player:
 		return
-		
+	
 	if rotating:
 		rotate_target = get_global_mouse_position()
-
+	
 	for unit in $Units.get_children():
 		unit.rotate_to(rotate_target)
 	$UnitDetection.look_at(rotate_target)
-
+	
 	Server.SendUnitState({unit_id:{'T':OS.get_system_time_msecs(),
 	  "P":get_global_position(), "R":rotate_target}})
 
